f4a330a8756491cae70e9d81a9182f0d34c5f203
saviorocha1313 <saviorocha1.3@gmail.com>
2025-04-12 23:55:29 -0300

    projeto iniciado

---
+++ src/test/java/com/savio/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savio.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
