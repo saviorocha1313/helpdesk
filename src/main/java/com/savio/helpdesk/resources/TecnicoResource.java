76a14502d5e8d09753623a3a5d6c3f0932cb48c7
saviorocha1313 <saviorocha1.3@gmail.com>
2025-07-15 19:45:39 -0300

    Validação nos campos de criação de novo usuario

--- src/main/java/com/savio/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/savio/helpdesk/resources/TecnicoResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,7 +41,7 @@ public class TecnicoResource {
 		return ResponseEntity.ok().body(listDTO);//fiz a requisição e vou retornar a lista de dto 
    }
 	@PostMapping
-	public ResponseEntity<TecnicoDTO> create(@RequestBody TecnicoDTO objDTO) {
+	public ResponseEntity<TecnicoDTO> create(@Valid @RequestBody TecnicoDTO objDTO) {
 		Tecnico newObj = service.create(objDTO);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
